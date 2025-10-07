b880b4f52674de44c3bddb764c8576f9de3f6774
Roberto <rmdero.j.r@gmail.com>
2025-10-07 10:34:59 -0300

    fix: Correct file paths for global installation

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,18 @@
 const path = require('path');
+const os = require('os');
+
+// Define a base directory in the user's home folder for the application data
+const appDataDir = path.resolve(os.homedir(), '.ibge-data-cli');
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      // The database file will be created at this path
-      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+      // The database file will be created in a dedicated folder in the user's home directory
+      filename: path.resolve(appDataDir, 'database.sqlite')
     },
     migrations: {
-      // Directory where migrations will be stored
+      // Migrations are read from the installation directory
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
     seeds: {
